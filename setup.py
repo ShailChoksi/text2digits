619f473ac64ece136a5fafd88b600d6704a924da
Shail Choksi <shail@pronavigator.io>
2019-03-30 23:07:23 -0400

    Prepare to upload to Pip

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="text2digits-careless25",
+    version="0.0.1",
+    author="Shail Choksi",
+    author_email="choksishail@gmail.com",
+    description="A small library to convert text numbers to digits in a string",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/careless25/text2digits",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
