de303f09572ed6fef808f3c73aaac23bde8bba02
Shail Choksi <shail@pronavigator.io>
2019-03-30 23:41:27 -0400

    Update setup.py with project urls and keywords

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="text2digits-careless25",
+    name="text2digits",
     version="0.0.2",
     author="Shail Choksi",
     author_email="choksishail@gmail.com",
@@ -13,6 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/careless25/text2digits",
     packages=setuptools.find_packages(),
+    keywords="text2numbers words2numbers digits numbers",
+    project_urls={
+        'Funding': "https://www.paypal.me/careless25",
+        'Source': "https://github.com/careless25/text2digits"
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
