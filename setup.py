674b6346f95771ecf4fa08b563c88d49ec31d3e5
Shail Choksi <shail@pronavigator.io>
2019-03-31 00:06:33 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="text2digits",
-    version="0.0.2",
+    version="0.0.3",
     author="Shail Choksi",
     author_email="choksishail@gmail.com",
     description="A small library to convert text numbers to digits in a string",
